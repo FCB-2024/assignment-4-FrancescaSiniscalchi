0e4f7664a61d2c85de209cc92ec8a742b2bc98b7
Francesca Siniscalchi <francesca.siniscalchi01@estudiant.upf.edu>
2024-10-22 22:56:17 +0200

    assignment-4-FrancescaSiniscalchi

--- src/antiprime.py
+++ src/antiprime.py
@@ -1,10 +1,34 @@
 ## ADD WHATEVER ARGUMENTS ARE NECESSARY TO THE MAIN FUNCTION
 ## IN THE SAME ORDER AS THE ARGUMENTS ARE TAKEN FROM THE
 ## COMMAND LINE SPECIFIED BELOW
-def main() :
+def main(x) :
+
 	## YOU CODE SHOULD START HERE AST THE SAME
 	## IDENTATION AS THIS COMMENT
+	x = 6
+	i = 1
+	r = 0
+	l = x - 1
+	z = 0
+	while i <= x :
+		if x % i == 0 :
+		   r = r + 1
+		i = i + 1
+    while l >= 1 :
+        a = 1
 
+        while a <= l :
+    	   if l % a == 0 :
+    	      z = z + 1
+    	    a = a + 1
+        if z >= r :
+           print ("not anti-prime")
+           l = 0
+        else:
+            l = l - 1
+            z = 0
+    if r > z :
+    	print ("anti-prime")
 
 	## THE LAST LINES OF YOUR CODE SHOULD EITHER
 	## RETURN THE VALUE "anti-prime" or "not anti-prime"
